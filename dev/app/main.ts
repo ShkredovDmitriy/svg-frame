e757041bb41a42dd2dc5c5b4e8455ce1e4d69beb
Shkredov Dmitriy <ShkredovDmitriy@gmail.com>
2021-10-25 16:52:21 +0300

    add docs

--- dev/app/main.ts
+++ dev/app/main.ts
@@ -1,4 +1,16 @@
 import "~/common/scss/main.scss";
 
 import svgFrame from "./../components/ds-svg-frame/svg-frame";
-svgFrame();
+svgFrame({
+  dots: [
+    "1,16",
+    "16,1",
+    "-16,1",
+    "100%,16",
+    "100%,60%",
+    "-10,60%+10",
+    "-10,-16",
+    "-26,100%",
+    "1,100%"
+  ]
+});
--- dev/components/ds-svg-frame/svg-frame.js
+++ dev/components/ds-svg-frame/svg-frame.js
@@ -7,27 +7,25 @@ const svgFrame = config => {
   let dots = defaultConfig.dots;
   let wrapper = defaultConfig.wrapper;
   const WrElement = document.querySelector(wrapper) || "";
-  // if (config) {
-  //   dots = config.dots || defaultConfig.dots;
-  //   wrapper = config.wrapper || defaultConfig.wrapper;
-  // }
+  if (config) {
+    dots = config.dots || defaultConfig.dots;
+    wrapper = config.wrapper || defaultConfig.wrapper;
+  }
 
   if (WrElement) {
     const resizeObserver = new ResizeObserver(entry => {
-      const w = entry[0].borderBoxSize.inlineSize || 0;
-      const h = entry[0].borderBoxSize.blockSize || 0;
-
-      console.log(entry[0]);
+      const w = WrElement.clientWidth || 0;
+      const h = WrElement.clientHeight || 0;
 
       const frame = dots.map(coord => {
         let x = coord.split(",")[0];
         let y = coord.split(",")[1];
         if (x === "w" || x === "100%") x = w - 1;
-        if (y === "h" || y === "100%") y = h - 1;
+        if (y === "h" || y === "100%") y = h - 2;
         if (x[x.length - 1] === "%") x = (w * parseInt(x)) / 100;
         if (y[y.length - 1] === "%") y = (h * parseInt(y)) / 100;
         if (y[2] + y[3] === "%+") {
-          t = y.split("%+");
+          const t = y.split("%+");
           y = (h * parseInt(t[0])) / 100 + parseInt(t[1]);
         }
         if (x[0] === "-") x = w + Number(x) || w;
@@ -35,6 +33,7 @@ const svgFrame = config => {
         x = Math.floor(x);
         return [x, y].join(",");
       });
+      // console.log(frame);
       const svg = `<svg class="frame" viewBox="0 0 ${w} ${h}"><polygon points="${frame}" fill="inherit" stroke-width="inherit" stroke="inherit"/></svg>`;
       WrElement.innerHTML = svg;
     });
